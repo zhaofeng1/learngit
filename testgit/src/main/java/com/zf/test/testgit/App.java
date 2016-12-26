a7f322bd43540c36cfcf00d35230896d0a315fcd
zhaofeng <zhaofeng@altamob.com>
2016-12-26 11:52:16 +0800

    first commit

---
+++ testgit/src/main/java/com/zf/test/testgit/App.java
@@ -0,0 +1,13 @@
+package com.zf.test.testgit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
